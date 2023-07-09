cfec3e35413739c2b98f1efc90c968454033adfb
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-08 18:00:03 -0700

    WIP 1st draft Friend component

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -3,16 +3,20 @@ import React from 'react';
 function Friend({ friend }) {
 	return (
 		<li>
-			<div>
-				<img alt="Some person's face" src={friend.image} />
-				<div>
-					<h2>{friend.name}</h2>
-					<p>
-						You owe {friend.name} ${friend.balance}
-					</p>
-				</div>
-				<button className='button'>Select</button>
-			</div>
+			<img alt="Some person's face" src={friend.image} />
+			<h3>{friend.name}</h3>
+			{friend.balance < 0 && (
+				<p className='red'>
+					You owe {friend.name} ${Math.abs(friend.balance)}
+				</p>
+			)}
+			{friend.balance > 0 && (
+				<p className='green'>
+					{friend.name} owes you ${Math.abs(friend.balance)}
+				</p>
+			)}
+			{friend.balance === 0 && <p>You and {friend.name} are even</p>}
+			<button className='button'>Select</button>
 		</li>
 	);
 }
